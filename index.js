a7f4dec006045dcbd0baa96f8d268bc4bcd5e3be
Damian Krzeminski <pirxpilot@furkot.com>
2025-06-15 09:13:03 -0600

    use `biome` as a linter and formatter

--- index.js
+++ index.js
@@ -1,51 +1,47 @@
-const assert = require('assert')
-const wayfarer = require('@pirxpilot/wayfarer')
+const assert = require('assert');
+const wayfarer = require('@pirxpilot/wayfarer');
 
-module.exports = nanorouter
+module.exports = nanorouter;
 
 // electron support
-const isLocalFile = (/file:\/\//.test(
-  typeof window === 'object' &&
-  window.location &&
-  window.location.origin
-))
+const isLocalFile = /file:\/\//.test(typeof window === 'object' && window.location && window.location.origin);
 
-const stripElectron = /^(file:\/\/|\/)(.*\.html?\/?)?/
-const prefix = /^(http(s)?(:\/\/))?(www\.)?[a-zA-Z0-9-_.]+(:[0-9]{1,5})?(\/{1})?/
-const normalize = /#/
-const suffix = /'[?].*$'/
+const stripElectron = /^(file:\/\/|\/)(.*\.html?\/?)?/;
+const prefix = /^(http(s)?(:\/\/))?(www\.)?[a-zA-Z0-9-_.]+(:[0-9]{1,5})?(\/{1})?/;
+const normalize = /#/;
+const suffix = /'[?].*$'/;
 
-function nanorouter (opts = {}) {
-  const router = wayfarer(opts.default || '/404')
+function nanorouter(opts = {}) {
+  const router = wayfarer(opts.default || '/404');
 
   return {
     on,
     emit,
     match
+  };
+
+  function on(routename, listener) {
+    assert(typeof routename === 'string');
+    routename = routename.replace(/^[#/]/, '');
+    router.on(routename, listener);
   }
 
-  function on (routename, listener) {
-    assert(typeof routename === 'string')
-    routename = routename.replace(/^[#/]/, '')
-    router.on(routename, listener)
+  function emit(routename) {
+    assert(typeof routename === 'string');
+    routename = pathname(routename, isLocalFile);
+    return router.emit(routename);
   }
 
-  function emit (routename) {
-    assert(typeof routename === 'string')
-    routename = pathname(routename, isLocalFile)
-    return router.emit(routename)
-  }
-
-  function match (routename) {
-    assert(typeof routename === 'string')
-    routename = pathname(routename, isLocalFile)
-    return router.match(routename)
+  function match(routename) {
+    assert(typeof routename === 'string');
+    routename = pathname(routename, isLocalFile);
+    return router.match(routename);
   }
 }
 
 // replace everything in a route but the pathname and hash
-function pathname (routename, isElectron) {
-  if (isElectron) routename = routename.replace(stripElectron, '')
-  else routename = routename.replace(prefix, '')
-  return decodeURI(routename.replace(suffix, '').replace(normalize, '/'))
+function pathname(routename, isElectron) {
+  if (isElectron) routename = routename.replace(stripElectron, '');
+  else routename = routename.replace(prefix, '');
+  return decodeURI(routename.replace(suffix, '').replace(normalize, '/'));
 }
